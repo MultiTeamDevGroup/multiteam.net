4bc1658d5f3253ce7a5b98f0208b14b3f14148ac
zal1000 <zal1000@users.noreply.github.com>
2022-01-23 20:21:15 +0000

    deploy: 727659af1dc35395d461e871d694e79a68406a02

---
+++ _nuxt/static/1642969275/games/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmultiteam.net\u002F_nuxt\u002Fstatic\u002F1642969275",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgames",config:{_app:{basePath:"\u002Fmultiteam.net\u002F",assetsPath:"\u002Fmultiteam.net\u002F_nuxt\u002F",cdnURL:a}}}}(null));
