2c3a1cc51bcb19d2bf1fc684b6aa14580d633820
Zalán Horváth <horvathzalan@zal1000.com>
2022-01-25 02:40:57 +0100

    Updated background to webp

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@
 <style scoped>
     .main {
         height: 100vh;
-        background-image: url("/assets/background.png");
+        background-image: url("/assets/background.webp");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
